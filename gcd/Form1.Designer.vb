9b7a94ec01f9d3853be54195f52315124c3eb581
gcallah <gcallah@mac.com>
2015-08-17 23:51:14 -0500

    "First commit."

---
+++ gcd/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FirstNumLbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FirstNumLbl
+        '
+        Me.FirstNumLbl.AutoSize = True
+        Me.FirstNumLbl.Location = New System.Drawing.Point(60, 95)
+        Me.FirstNumLbl.Name = "FirstNumLbl"
+        Me.FirstNumLbl.Size = New System.Drawing.Size(91, 17)
+        Me.FirstNumLbl.TabIndex = 0
+        Me.FirstNumLbl.Text = "First number:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.FirstNumLbl)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FirstNumLbl As Label
+End Class
