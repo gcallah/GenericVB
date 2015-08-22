e580bb7d7f618f392175fa68652aa5e58e8a97c1
gcallah <gcallah@mac.com>
2015-08-22 17:39:13 -0400

    Moved math funcs off to separate module.

--- gcd/Form1.vb
+++ gcd/Form1.vb
@@ -1,23 +1,12 @@
-﻿Public Class Form1
+﻿Imports MathFuncs = gcd.math
+
+Public Class Form1
     Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
         Dim num1, num2 As Integer
         num1 = CInt(FirstNumBox.Text)
         num2 = CInt(ScndNumBox.Text)
-        AnswerBox.Text = gcd(num1, num2)
+        AnswerBox.Text = MathFuncs.gcd(num1, num2)
     End Sub
 
-    Function gcd(ByVal a, ByVal b) As Integer
-        Do While (b <> 0)
-            a = a Mod b
-            Swap(a, b)
-        Loop
-        Return a
-    End Function
-
-    Public Sub Swap(ByRef a As Integer, ByRef b As Integer)
-        Dim c = b
-        b = a
-        a = c
-    End Sub
 End Class
 
---
+++ gcd/Math.vb
@@ -0,0 +1,21 @@
+﻿Module Math
+
+    Public Class MathFuncs
+
+        Public Shared Function gcd(ByVal a, ByVal b) As Integer
+            Do While (b <> 0)
+                a = a Mod b
+                Swap(a, b)
+                ' Swap(a, b)
+            Loop
+            Return a
+        End Function
+
+        Public Shared Sub Swap(Of T)(ByRef a As T, ByRef b As T)
+            Dim c As T
+            c = a
+            a = b
+            b = c
+        End Sub
+    End Class
+End Module
