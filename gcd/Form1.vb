42e98e4f04d268c6cf38f1ca42bec7a114e7d95d
gcallah <gcallah@mac.com>
2015-08-22 15:32:00 -0400

    GCD working

--- gcd/Form1.vb
+++ gcd/Form1.vb
@@ -1,10 +1,23 @@
 ﻿Public Class Form1
     Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
-        Dim num1 As Integer
-        Dim num2 As Integer
+        Dim num1, num2 As Integer
         num1 = CInt(FirstNumBox.Text)
         num2 = CInt(ScndNumBox.Text)
-        AnswerBox.Text = num1 / num2
+        AnswerBox.Text = gcd(num1, num2)
+    End Sub
+
+    Function gcd(ByVal a, ByVal b) As Integer
+        Do While (b <> 0)
+            a = a Mod b
+            Swap(a, b)
+        Loop
+        Return a
+    End Function
+
+    Public Sub Swap(ByRef a As Integer, ByRef b As Integer)
+        Dim c = b
+        b = a
+        a = c
     End Sub
 End Class
 
